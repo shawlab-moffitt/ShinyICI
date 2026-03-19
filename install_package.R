6071c104022e7ec05b21f7eb1876715f51d81956
sgrtao3 <122512334+sgrtao3@users.noreply.github.com>
2026-03-19 15:03:35 -0400

    Update install_package.R

--- install_package.R
+++ install_package.R
@@ -1,2 +1,2 @@
-install.packages("G:/Ranran/ICB_Response/github_ready_shiny/ICBResponse_0.1.0.tar.gz", repos = NULL, type = "source")
-ICBResponse::run_app(data_dir = "G:/Ranran/ICB_Response/github_ready_shiny/inst/app/data")
+remotes::install_github("sgrtao3/ShinyICI", force = TRUE)
+ICBResponse::run_app()
